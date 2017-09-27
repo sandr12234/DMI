b398328d49492bccac4bd7aa020d45341e653b62
Sandris <sandr12234@gmail.com>
2017-09-27 11:47:08 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-NAME="Vaards Uzvaards"
+
+#4.piemers
+echo "Tiek pildiits skripts: "$0
+#echo $0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: " $#
+echo "SKriptam nodotie argumenti (attelosana/grupesana 1): "$*
+echo "Skriptam nodotie argumenti (attelosana/grupesana 2): "$@
+echo $?
+echo "Skriptam pieskirtais procesa numurs: " $$
+echo ""$!
+
+#3. piemers
+#NAME="Vaards Uzvaards"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
-
+#2. piemers
 #readonly NAME
 #echo $NAME
 #NAME="Uzvaards Vaards"
 #echo $NAME
 
+#1. piemers
+#NAME="V=Vaards Uzvaards"
+#echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
