165778d47751e8e7bfd7f49458b19408e171fe3c
Sandris <sandr12234@gmail.com>
2017-10-24 23:04:51 +0300

    20172410_23_04

---
+++ uzd2.sh
@@ -0,0 +1,22 @@
+#1/bin/bash 
+
+#area[11]=23
+#area[13]=37
+#area[51]=Ufo
+
+#echo ${area[11]}
+
+#area[1]=`expr ${area[11]} + ${area[13]} `
+#echo ${area[1]}
+
+#area2=( 1 3 4 5 )
+#echo ${area2[3]}
+
+echo "1."
+read a
+echo "2."
+read b
+echo "3."
+read c
+area1=( $a $b $c )
+echo ${area1[2]}
